491508e1c990dc5a4678d641a716eb2bc54a542c
peterchang <jy02927731@yahoo.com.hk>
2018-07-20 11:00:05 +0800

    Update cronBasicExample.js

--- cronBasicExample.js
+++ cronBasicExample.js
@@ -1,12 +1,12 @@
 var cron = require('node-cron');
 
-//exceute every 2:30am morning
+//execute every 2:30am morning
 // cron.schedule('30 2 * * *', function(){
 //  console.log('running a task every two minutes');
 // });
 
 
-//exceute every 1 min
+//execute every 1 min
 cron.schedule('*/1 * * * *', function(){
     console.log('running a task every two minutes');
 });
